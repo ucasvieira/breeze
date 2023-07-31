fa3f2d9bcb0ff79a37d378331a52fca745a0a063
ucasvieira <lucas.souza1012@etec.sp.gov.br>
2023-07-31 09:16:41 -0300

    first

---
+++ resources/views/cadastro.blade.php
@@ -0,0 +1,69 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Cadastrar Cliente') }}
+        </h2>
+    </x-slot>
+    
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">    
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <!-- Name -->
+        
+        <div>
+            <x-input-label for="name" :value="__('Nome')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+        
+        <!-- Endereço -->
+        
+        <div>
+            <x-input-label for="name" :value="__('Endereço')" class="mt-8"/>
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+        
+        <!-- Bairro -->
+        
+        <div>
+            <x-input-label for="name" :value="__('Bairro')"  class="mt-8"/>
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+        
+        <!-- Cep -->
+        
+        <div>
+            <x-input-label for="name" :value="__('Cep')"  class="mt-8"/>
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+        
+        <!-- Cidade -->
+        
+        <div>
+            <x-input-label for="name" :value="__('Cidade')"  class="mt-8"/>
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+
+            <x-primary-button class="ml-4">
+                {{ __('Register') }}
+            </x-primary-button>
+        </div>
+    </form>
+    </div>
+            </div>
+        </div>
+        </div>
+    
+ 
+
+</x-app-layout>
